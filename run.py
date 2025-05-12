dea4dc07b7de7984f5e393eed52c33672ed66d43
Glauber kaoly <kaolydantas@gmail.com>
2025-05-12 19:22:26 -0300

    Update run.py

--- run.py
+++ run.py
@@ -22,6 +22,6 @@ if __name__ == "__main__":
     print("starting flask...")
     time.sleep(1)
 
-    # Inicia o Flask
+    # start flask
     app.run(host="0.0.0.0", port=5000)
     print("flask running...")
